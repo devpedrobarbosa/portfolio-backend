be7d850d5ecffde1cff937a53fd8dd9432a7fec8
devpedrobarbosa <1230110831@veigadealmeida.edu.br>
2025-03-24 22:51:44 -0300

    fixed api

--- src/main/java/dev/pedrao/portfolio_api/config/CorsConfig.java
+++ src/main/java/dev/pedrao/portfolio_api/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package dev.pedrao.portfolio_api.config;
 
+import lombok.NonNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -12,15 +13,12 @@ public class CorsConfig implements WebMvcConfigurer {
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://www.pedrao.tech", "http://localhost:3000")
-                        .allowedMethods("POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedOrigins("https://www.pedrao.tech")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
                         .allowCredentials(true);
-
-                registry.addMapping("/**")
-                        .allowedMethods("GET")
-                        .allowedOrigins("*");
             }
         };
     }
